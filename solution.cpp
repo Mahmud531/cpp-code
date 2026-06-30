3b8587975e4d81ff2c117bccf9131c8077b73c11
Mahmud531 <mahmoodhossain0032@gmail.com>
2026-06-30 20:04:55 +0600

    added main template

--- solution.cpp
+++ solution.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 void binarySearch()
-{ // code }
-    int main()
-    {
-        ios::sync_with_stdio;
-        cin.tie(0);
-        cout.tie(0);
+{
+    int low = 0, high = 1e5;
+    int mid = (low + high) / 2;
+}
+int main()
+{
+    ios::sync_with_stdio;
+    cin.tie(0);
+    cout.tie(0);
 
-        std::cout << "hello";
-        return 0;
-    }
+    std::cout << "hello world";
+    return 0;
+}
